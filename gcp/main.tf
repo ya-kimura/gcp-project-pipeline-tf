87dd1e7c6127e61b158d8cf6c47460b0a4030072
ya.kimura <yassuifortes@outlook.com>
2022-10-03 17:08:16 -0300

    ci: testing tfsec

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,25 +1,6 @@
-resource "google_composer_environment" "test" {
-  name   = "example-composer-env"
-  region = "us-central1"
-  config {
-    node_count = 4
-
-    node_config {
-      zone         = "us-central1-a"
-      machine_type = "n1-standard-1"
-
-      network    = google_compute_network.test.id
-      subnetwork = google_compute_subnetwork.test.id
-
-      service_account = google_service_account.test.name
-    }
-
-    database_config {
-      machine_type = "db-n1-standarAb44"
-    }
-
-    web_server_config {
-      machine_type = "123495"
-    }
+resource "google_compute_firewall" "bad_example" {
+  source_ranges = ["0.0.0.0/0"]
+  allow {
+    protocol = "icmp"
   }
 }
